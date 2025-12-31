df60c685b7bb2cafec5962fd17a519fb5cceb97e
shahdqou15 <shahd.qou15@gmail.com>
2025-12-31 21:22:52 +0200

    git 10 products at home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,34 @@ const displayCategories = async () => {
 }
 
 displayCategories();
+
+
+
+
+
+const getProducts = async()=>{
+    const response = await axios.get(`https://dummyjson.com/products?limit=10`);
+    return response.data.products;
+}
+
+const displayProducts = async () => {
+
+    const result = await getProducts();
+    const products = result.map((product) => {
+        return `
+        <div class="col-md-4 col-lg-3 mb-4">
+            <div class="card h-100 product-card">
+                <img src="${product.thumbnail}" class="card-img-top" alt="product-image">
+                    <div class="card-body">
+                        <h6 class="card-title">${product.title}</h6>
+                        <p class="fw-bold">${product.price}</p>
+                    </div>
+            </div>
+        </div>`       
+
+    }).join(" ")
+    
+    document.querySelector(".products .row").innerHTML= products;
+}
+
+displayProducts();
