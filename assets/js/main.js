705a9c1090f0be478ebf86e0c0cd515f61e8b031
shahdqou15 <shahd.qou15@gmail.com>
2026-01-02 22:06:10 +0200

    add modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,11 @@ const displayCategories = async () => {
                     </div>
                  </div>
             </a>
-        </div>`       
+        </div>`
 
     }).join(" ")
-    
-    document.querySelector(".categories .row").innerHTML= categories;
+
+    document.querySelector(".categories .row").innerHTML = categories;
 }
 
 displayCategories();
@@ -30,7 +30,7 @@ displayCategories();
 
 
 
-const getProducts = async()=>{
+const getProducts = async () => {
     const response = await axios.get(`https://dummyjson.com/products?limit=10`);
     return response.data.products;
 }
@@ -41,18 +41,69 @@ const displayProducts = async () => {
     const products = result.map((product) => {
         return `
         <div class="col-md-4 col-lg-3 mb-4">
-            <div class="card h-100 product-card">
-                <img src="${product.thumbnail}" class="card-img-top" alt="product-image">
+            <div class="card h-100 product-card" data-id="${product.id}">
+                <img src="${product.thumbnail}" class="card-img-top product-img " alt="product-image">
                     <div class="card-body">
-                        <h6 class="card-title">${product.title}</h6>
-                        <p class="fw-bold">${product.price}</p>
+                        <h6 class="card-title fw-semibold text-danger-emphasis">${product.title}</h6>
+                        <p class="fw-bold ">$${product.price}</p>
                     </div>
             </div>
-        </div>`       
+        </div>`
 
     }).join(" ")
-    
-    document.querySelector(".products .row").innerHTML= products;
-}
+
+    document.querySelector(".products .row").innerHTML = products;
+
+
+
+
+
+
+
+};
+
 
 displayProducts();
+
+
+
+
+const getProductDetails = async (id) => {
+    const response = await axios.get(`https://dummyjson.com/products/${id}`);
+    return response.data;
+};
+
+const showProductDetails = async (id) => {
+    const myModal = document.querySelector(".my-modal");
+    const product = await getProductDetails(id);
+    const closeBtn = document.querySelector(".close-btn");
+    
+    document.querySelector(".my-modal").classList.remove("d-none");
+    document.querySelector(".details-image").src = product.thumbnail;
+    document.querySelector(".details-title").textContent = product.title;
+    document.querySelector(".details-description").textContent = product.description;
+    document.querySelector(".details-price").textContent = product.price;
+    document.querySelector(".details-category").textContent = product.category;
+    document.querySelector(".product-details").classList.remove("d-none");
+
+    const closeModal = () => {
+        myModal.classList.add("d-none");
+    };
+    closeBtn.addEventListener("click", () => {
+        closeModal();
+    });
+};
+
+
+document.querySelector(".products .row").addEventListener("click", (e) => {
+    const card = e.target.closest(".product-card");
+    if (!card) return;
+
+    const id = card.dataset.id;
+    showProductDetails(id);
+});
+
+
+
+
+
